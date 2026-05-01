fb7ac97bc22d2caa1f88f22711069e72cad5e6a3
ma-23 <mahmoodabuhassnah@gmail.com>
2026-05-01 18:09:22 +0300

    finished

--- api_central.js
+++ api_central.js
@@ -3,11 +3,7 @@ const app = require("./app.js");
 const {Pool} = require("pg");
 require("dotenv").config();
 const pool = new Pool({
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    port: Number(process.env.DATABASE_PORT),
-    host: process.env.DATABASE_HOST,
-    database: process.env.DATABASE_NAME
+    connectionString:process.env.CONNCECTION_STRING
 
 });
 const create_table = async () =>{
