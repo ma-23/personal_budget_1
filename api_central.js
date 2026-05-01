ce8535aa26f9df9077a1ac8d7d3a4d7437561b94
ma-23 <mahmoodabuhassnah@gmail.com>
2026-05-01 17:40:28 +0300

    Used a database instead of an array for storing data

--- api_central.js
+++ api_central.js
@@ -1,18 +1,31 @@
 const express = require("express");
 const app = require("./app.js");
+const {Pool} = require("pg");
+require("dotenv").config();
+const pool = new Pool({
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    port: Number(process.env.DATABASE_PORT),
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME
+
+});
+const create_table = async () =>{
+await pool.query("create table if not exists envolope(id integer primary key, title text default 'TBD', amount integer default 0)");
+}
+create_table();
 /*
-We are going to do a couple of things. First, we will create an array that represent a place of storage. Normally that would a database but an array will work here
+We are going to do a couple of things. First, we will connect to the database in render that represent a place of storage.
 Second, we  are not going to create other routers, we will be using app directly; Because the app functionality 
 is not that complex to require that.
 */
 
-//Create the array holder for messages
-let envolope = [];
+
 //The structure of the envolopes is: Id(number),Title(String),amount(Number)
 
 const check_body = (req,res,next) =>{
 
-    //We are going to assume that the information is in the body of the  request.
+    //We are going to assume that the information is in the body of the  request. Assuming that it's a json object
     const body = req.body;
     // We have to see weather all the information is there.
     const {id} = body
@@ -52,54 +65,54 @@ const check_body = (req,res,next) =>{
 
 
 
-app.post("/envolope",check_body,(req,res,next) =>{
+app.post("/envolope",check_body, async (req,res,next) =>{
+    try{
     //First chect weather or not id already exist in the list
-    let exist = false;
-    for(let envolp of envolope){
-        if(req.id === envolp.id){
-            exist = true;
-            break;
-        }
-    }
+   let  result = await pool.query("select * from envolope where id = $1",[req.id]);
 
-    if(!exist){
+
+    if(result.rowCount === 0){
 
 //create the object, send it to the array, then send the appropriate message.
-const object = {id:req.id,title: req.title, amount: req.amount};
-envolope.push(object);
-res.status(201).send(object);
+  await pool.query("insert into envolope(id,title,amount) values($1,$2,$3)",[req.id,req.title,req.amount]);
+res.status(201).send("envolope saved");
     }
     else{
         res.status(500).send("envolope already exist");
     }
-
-
-});
-
-
-
-app.get("/envolope" ,(req,res,next) =>{
-
-res.send(envolope);
+    
+    }
+    catch(err){
+        next(err);
+    }
+   
 
 });
 
 
-app.get("/envolope/:id",(req,res,next) => {
 
-const envolope_id = Number(req.params.id);
-let  found_object = null;
-//use the find method in order to find the envolope.
-for(let i of envolope){
-if(i.id === envolope_id){
-    found_object = i;
-    break;
-}
-
+app.get("/envolope" , async (req,res,next) =>{
+try{
+//Query all the rows from the envolopes table and send the array as a json
+const result = await pool.query("select * from envolope order by id desc");
+res.json(result.rows);
 
 }
-if(found_object){
-res.send(found_object);
+catch(err){
+    next(err);
+}
+
+});
+
+
+app.get("/envolope/:id", async(req,res,next) => {
+
+const query = await pool.query("select * from envolope where id = $1",[Number(req.params.id)]);
+
+
+
+if(query.rowCount !==0){
+res.json(query.rows[0]);
 }
 else{
     res.status(404).send("envolope Don't exist");
@@ -110,21 +123,20 @@ else{
 
 
 
-app.put("/envolope/:id/:amount",(req,res,next) =>{
-const id = Number(req.params.id);
-const amount = Number(req.params.amount);
-//First, you look for the envolope.
-const Envolope = envolope.find((envolp) =>{
-return envolp.id === id;
+app.put("/envolope/:id/:amount",async (req,res,next) =>{
+let query = await pool.query("select * from envolope where id = $1",[Number(req.params.id)]);
 
-});
-
-if(Envolope.amount === 0 || amount > Envolope.amount){
+if(query.rowCount === 0){
+    res.status(404).send("Envolope not found");
+}
+else if(query.rows[0].amount === 0 || Number(req.params.amount) > query.rows[0].amount){
     res.status(404).send("Not allowed to take money.");
 }
 else{
-Envolope.amount -= amount;
-res.status(201).send(Envolope);
+const new_amount = query.rows[0].amount - Number(req.params.amount);
+query = await pool.query("update  envolope  set amount = $1 where id = $2",[new_amount,Number(req.params.id)]);
+query = await pool.query("select *  from envolope where id = $1",[Number(req.params.id)]);
+res.status(201).json(query.rows[0]);
 
 
 
@@ -138,21 +150,29 @@ res.status(201).send(Envolope);
 
 
 
-app.delete("/envolope/:id",(req,res,next)=>{
-const id = Number(req.params.id);
-envolope = envolope.filter((ele) =>{
+app.delete("/envolope/:id",async (req,res,next)=>{
+// First check if it exist
+let query = await pool.query("select * from envolope where id = $1",[Number(req.params.id)]);
 
-return ele.id !== id
+if(query.rowCount === 0){
+    res.status(404).send("Envolope not found");
+}
+else{
 
-});
+
+ query = await pool.query("delete from envolope where id = $1",[Number(req.params.id)]);
 
 res.status(204).send();
+}
 
 });
 
 
+app.use((err,req,res,next) =>{
 
-
+console.log(err.stack);
+res.status(500).send(err.stack);
+});
 
 
 
--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express  = require("express");
 const app = express();
 const parser = require("body-parser");
 const morgan = require("morgan");
+const cors = require("cors");
 //From here the app will start, but the listening part will be in the main.js file.
 
 //We will have a body parser that will  parse the json object into a js object under req.body.
@@ -10,7 +11,7 @@ app.use(parser.json());
 //for logging purposes.
 app.use(morgan("tiny"));
 
-
+app.use(cors());
 
 
 
